84e1d464a7658410878897d287dc372beea6ad97
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2021-12-22 16:53:36 +0700

    implement e2e kustomize
    Signed-off-by: zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>

--- e2e/scripts/main.py
+++ e2e/scripts/main.py
@@ -4,13 +4,20 @@ import os
 
 from deployer.kustomize import KustomizeDeployer
 from deployer.shell import ShellDeployer
+from deployer.manifest import ManifestDeployer
+
+DEFAULT_CLUSTER_MANIFEST_DIR = "manifests/"
 
 def main():
     os.chdir("e2e")
 
     shell = ShellDeployer()
+
     kustomize = KustomizeDeployer(shell)
     kustomize.deploy()
+    
+    manifest = ManifestDeployer(shell, DEFAULT_CLUSTER_MANIFEST_DIR)
+    manifest.deploy()
 
 if __name__ == '__main__':
     main()
