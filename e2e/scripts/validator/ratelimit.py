d66f0247dfadd1b958477cd6027680d2005119de
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2021-12-24 02:23:27 +0700

    fix
    Signed-off-by: zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>

--- e2e/scripts/validator/ratelimit.py
+++ e2e/scripts/validator/ratelimit.py
@@ -18,14 +18,14 @@ class RatelimitValidator():
             validate_command = ["kubectl", "-n", "development", "exec", "-i", "deploy/client", "-c", "client",
                                  "--", "curl", "http://istio-ingressgateway.istio-system.svc.cluster.local:80%s" %(path), "-H", "'Host:", "%s'" %(domain), "--write-out", "'%{json}'"]
             out = self.shell.os_execute(' '.join(validate_command))
-            if '"http_code":429' not in ''.join(out):
+            if '"http_code":429' not in out:
                 raise Exception("it's not ratelimited") 
         
         else:
             validate_command = ["kubectl", "-n", "development", "exec", "-i", "deploy/client", "-c", "client",
                               "--", "curl", "http://%s:9898%s" %(domain, path), "-H", "'Host:", "%s'" %(domain), "--write-out", "'%{json}'"]
-            out = self.shell.os_execute(validate_command)
-            if '"http_code":429' not in ''.join(out):
+            out = self.shell.os_execute(' '.join(validate_command))
+            if '"http_code":429' not in out:
                 raise Exception("it's not ratelimited") 
             
         print('ratelimit is working!')
