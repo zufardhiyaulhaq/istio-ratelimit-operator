27eea48bc490ea6469a851888e988926b7f8019f
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2021-12-23 21:25:38 +0700

    add response code in error
    Signed-off-by: zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>

--- e2e/scripts/validator/ratelimit.py
+++ e2e/scripts/validator/ratelimit.py
@@ -11,6 +11,6 @@ class RatelimitValidator():
 
         response = requests.get('http://localhost:8080%s' % path, headers=headers)
         if response.status_code != 429:
-            raise Exception("It's not ratelimited") 
+            raise Exception("response code: %d, it's not ratelimited" % response.status_code) 
         
-        print('Ratelimit works!')
+        print('ratelimit is working!')
