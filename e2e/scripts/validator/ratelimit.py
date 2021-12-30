6c3ec7ffb6da49db0238412653d8ea0fd29c50dd
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2021-12-30 15:09:13 +0700

    check validator
    Signed-off-by: zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>

--- e2e/scripts/validator/ratelimit.py
+++ e2e/scripts/validator/ratelimit.py
@@ -8,10 +8,11 @@ class RatelimitValidator():
     
     def validate(self, domain, path, retry):
         if self.gateway:
+            print("validate using port forward")
             headers = {
                 'Host': domain,
             }
-
+            
             for sequence in range(retry):
                 response = requests.get('http://localhost:8080%s' % path, headers=headers)
                 if response.status_code != 429:
@@ -19,11 +20,13 @@ class RatelimitValidator():
                         continue
                     raise Exception("response code: %d, it's not ratelimited" % response.status_code) 
             
+            print("validate using port kubectl exec")
             validate_command = ["kubectl", "-n", "development", "exec", "-i", "deploy/client", "-c", "client",
                                  "--", "curl", "http://istio-ingressgateway.istio-system.svc.cluster.local:80%s" %(path), "-H", "'Host:", "%s'" %(domain), "--write-out", "'%{json}'"]
             
             for sequence in range(retry):
                 out = self.shell.os_execute(' '.join(validate_command))
+                print(' '.join(validate_command))
                 print(out)
                 if '"http_code":429' not in out:
                     if sequence is not retry:
@@ -31,6 +34,7 @@ class RatelimitValidator():
                     raise Exception("it's not ratelimited") 
         
         else:
+            print("validate using port kubectl exec")
             validate_command = ["kubectl", "-n", "development", "exec", "-i", "deploy/client", "-c", "client",
                               "--", "curl", "http://%s:9898%s" %(domain, path), "-H", "'Host:", "%s'" %(domain), "--write-out", "'%{json}'"]
             for sequence in range(retry):
