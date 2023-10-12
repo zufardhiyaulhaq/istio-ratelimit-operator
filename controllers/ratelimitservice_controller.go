ce5fdfa0f5222c2af6564e37203626978f746def
zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>
2023-10-12 15:12:20 +0700

    fix unsupported protocol scheme when calling reload statsd-exporter
    Signed-off-by: zufardhiyaulhaq <zufardhiyaulhaq@gmail.com>

--- controllers/ratelimitservice_controller.go
+++ controllers/ratelimitservice_controller.go
@@ -50,7 +50,7 @@ func reloadStatsdExporter(domain string) error {
 		Timeout: 5 * time.Second,
 	}
 
-	response, err := client.Post(domain+":9102/-/reload", "", nil)
+	response, err := client.Post("http://"+domain+":9102/-/reload", "", nil)
 	if err != nil {
 		return err
 	}
